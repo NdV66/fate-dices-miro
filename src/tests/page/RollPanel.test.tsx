48f8fe16cce377c14e45d848abf700a6a62047e1
Marta Zazlak <marta.zazlak@hotmail.com>
2022-12-24 10:29:37 +0100

    tests

--- src/tests/page/RollPanel.test.tsx
+++ src/tests/page/RollPanel.test.tsx
@@ -2,8 +2,13 @@ import { fireEvent, render } from '@testing-library/react';
 import '@testing-library/jest-dom';
 
 import * as modificationsService from '../../services/testIfModIsOk';
+import * as fateDicesService from '../../services/fateDices';
+import * as rollsService from '../../services/rolls';
+import * as translateFateRollsView from '../../views/translateFateRolls';
+
 import { RollPanel } from '../../page';
-import { TEXTS } from '../../const';
+import { FATE, TEXTS } from '../../const';
+import { FateDicesType } from '../../types';
 
 const CHECKBOX_SELECTOR = 'input[type="checkbox"]';
 const INPUT_SELECTOR = 'input[type="text"]';
@@ -30,74 +35,57 @@ describe('RollPanel', () => {
     });
 
     describe('Roll button behavior when input is showed', () => {
-        const getButton = (value: string) => {
+        const prepareGivenStep = (value: string) => {
             const element = renderElement();
             const container = element.container;
-            let button = element.getByText(TEXTS.ROLL_BUTTON);
+            const button = element.getByText(TEXTS.ROLL_BUTTON);
 
             const toggle = container.querySelector(CHECKBOX_SELECTOR);
             fireEvent.click(toggle!);
 
             const input = container.querySelector(INPUT_SELECTOR);
-            fireEvent.change(input!, { target: { value: '6' } });
+            fireEvent.change(input!, { target: { value } });
 
-            return button;
+            return { button, element, input };
         };
 
         test('Should be able to click, if input value is ok', () => {
             jest.spyOn(modificationsService, 'testIfModIsOk').mockReturnValue(true);
+            const { button } = prepareGivenStep('6');
 
-            const element = renderElement();
-            const container = element.container;
-            let button = element.getByText(TEXTS.ROLL_BUTTON);
-
-            const toggle = container.querySelector(CHECKBOX_SELECTOR);
-            fireEvent.click(toggle!);
-
-            const input = container.querySelector(INPUT_SELECTOR);
-            fireEvent.change(input!, { target: { value: '6' } });
-
-            fireEvent.click(button);
-            button = element.getByText(TEXTS.ROLL_BUTTON);
             expect(button).not.toBeDisabled();
         });
 
         test('Should disabled, if input value is not ok', () => {
             jest.spyOn(modificationsService, 'testIfModIsOk').mockReturnValue(false);
+            const { button } = prepareGivenStep('a6');
 
-            const element = renderElement();
-            const container = element.container;
-            let button = element.getByText(TEXTS.ROLL_BUTTON);
-
-            const toggle = container.querySelector(CHECKBOX_SELECTOR);
-            fireEvent.click(toggle!);
-
-            const input = container.querySelector(INPUT_SELECTOR);
-            fireEvent.change(input!, { target: { value: 'a6' } });
-
-            fireEvent.click(button);
-            button = element.getByText(TEXTS.ROLL_BUTTON);
             expect(button).toBeDisabled();
         });
 
-        test('Should disabled, if input value is not ok - and be active, when value is changed to ok', () => {
+        test('Should disabled, if an input value is not ok - and be active, when the value is changed to ok', () => {
             jest.spyOn(modificationsService, 'testIfModIsOk').mockReturnValueOnce(false);
+            const { button, input } = prepareGivenStep('a6');
 
-            const element = renderElement();
-            const container = element.container;
-            let button = element.getByText(TEXTS.ROLL_BUTTON);
-
-            const toggle = container.querySelector(CHECKBOX_SELECTOR);
-            fireEvent.click(toggle!);
-
-            const input = container.querySelector(INPUT_SELECTOR);
-            fireEvent.change(input!, { target: { value: 'a6' } });
-
-            fireEvent.click(button);
-            button = element.getByText(TEXTS.ROLL_BUTTON);
             expect(button).toBeDisabled();
 
             jest.spyOn(modificationsService, 'testIfModIsOk').mockReturnValueOnce(true);
+            fireEvent.change(input!, { target: { value: '6' } });
+
+            expect(button).not.toBeDisabled();
+        });
+
+        test('Should be active if an input value is ok - and be disabled,  when the value is changed to the wrong one', () => {
+            jest.spyOn(modificationsService, 'testIfModIsOk').mockReturnValueOnce(true);
+            const { button, element } = prepareGivenStep('6');
+
+            expect(button).not.toBeDisabled();
+
+            jest.spyOn(modificationsService, 'testIfModIsOk').mockReturnValueOnce(false);
+            const input = element.container.querySelector(INPUT_SELECTOR);
+            fireEvent.change(input!, { target: { value: '+6' } });
+
+            expect(button).toBeDisabled();
         });
     });
 
@@ -122,17 +110,55 @@ describe('RollPanel', () => {
             const toggle = container.querySelector(CHECKBOX_SELECTOR);
             fireEvent.click(toggle!);
 
-            let input = container.querySelector(INPUT_SELECTOR);
+            const input = container.querySelector(INPUT_SELECTOR);
             expect(input).toBeInTheDocument();
 
             fireEvent.click(toggle!);
-
-            input = container.querySelector(INPUT_SELECTOR);
             expect(input).not.toBeInTheDocument();
+        });
+    });
+
+    describe('Result', () => {
+        const mockRolls = () => {
+            const rollValues: FateDicesType = [1, 0, -1, 0];
+            const fateRollValues = [FATE.TEXTS.PLUS, FATE.TEXTS.NEUTRAL, FATE.TEXTS.MINUS, FATE.TEXTS.NEUTRAL];
+
+            jest.spyOn(fateDicesService, 'rollFateDices').mockReturnValueOnce(rollValues);
+            jest.spyOn(rollsService, 'calcSummaryRolls').mockReturnValueOnce(2);
+            jest.spyOn(translateFateRollsView, 'translateFateRolls').mockReturnValueOnce(fateRollValues);
+        };
+
+        test('Should be not showed on enter', () => {
+            const result = renderElement().container.querySelector('.app-card');
+            expect(result).not.toBeInTheDocument();
+        });
+
+        test('Should display rolls (Result element)', () => {
+            mockRolls();
+
+            const element = renderElement();
+            const button = element.getByText(TEXTS.ROLL_BUTTON);
+            fireEvent.click(button);
+
+            const rolls = element.container.querySelector('.app-card');
+            expect(rolls).toBeInTheDocument();
+        });
+
+        test('Should display rolls (Result element) and then hide, when the clean button is clicked', () => {
+            mockRolls();
+
+            const element = renderElement();
+            const button = element.getByText(TEXTS.ROLL_BUTTON);
+            const cleanButton = element.getByText(TEXTS.CLEAN_BUTTON);
+            fireEvent.click(button);
+
+            const rolls = element.container.querySelector('.app-card');
+            expect(rolls).toBeInTheDocument();
+
+            fireEvent.click(cleanButton);
+            expect(rolls).not.toBeInTheDocument();
         });
     });
 });
 
 export {};
-
-//rolls
